1211fc642830526e2b8b4b435a20538137649907
Sergei <k5ii@ya.ru>
2020-12-03 13:27:20 +0300

    adding pytest

--- test_rerun.py
+++
@@ -1,9 +0,0 @@
-import time
-link = "http://selenium1py.pythonanywhere.com/"
-
-
-def test_guest_should_see_login_link_pass(browser):
-    browser.get(link)
-    time.sleep(4)
-    browser.find_element_by_css_selector("#login_link")
-
