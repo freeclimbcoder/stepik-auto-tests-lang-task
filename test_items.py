c694e0283890014cb3af4eb3614fb763bc55fdee
Sergei <k5ii@ya.ru>
2020-12-03 19:06:55 +0300

    localisation in comments fixed

--- test_items.py
+++ test_items.py
@@ -7,4 +7,4 @@ def test_isthere_a_(browser):
     browser.get(link)
     #time.sleep(5)
     assert browser.find_element_by_class_name('btn-add-to-basket').is_displayed(), \
-        'Кнопка добавления товара в корзину отсутсвует'
+        'I see no btn-add-to-basket button'
